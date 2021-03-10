103f82b0177d06121de41377c8ed35305c511b4c
xaviguasch <xaviguasch@gmail.com>
2021-03-10 20:25:27 +0100

    added solution to the challenge

--- index.js
+++ index.js
@@ -16,3 +16,18 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 
 GOOD LUCK 😀
 */
+
+const calcAverageHumanAge = (ages) => {
+  const humanAges = ages.map((age) => (age <= 2 ? (age = 2 * age) : (age = 16 + age * 4)))
+
+  const adults = humanAges.filter((age) => age >= 18)
+
+  const average = adults.reduce((acc, age) => acc + age, 0) / adults.length
+
+  return average
+}
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3])
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4])
+
+console.log(avg1, avg2)
